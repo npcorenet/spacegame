5c13ccca54c2ee3697632fb83dee0cbdee07a82a
namePlayer <r06sollfrank@gmail.com>
2022-07-14 13:32:31 +0200

    Add Register Fields Test

--- tests/Validation/RegisterFieldValidationTest.php
+++ tests/Validation/RegisterFieldValidationTest.php
@@ -2,6 +2,8 @@
 
 namespace Validation;
 
+use App\Helper\MessageHelper;
+use App\Model\AccountModel;
 use App\Validation\RegisterFieldValidation;
 use PHPUnit\Framework\TestCase;
 
@@ -9,12 +11,28 @@ class RegisterFieldValidationTest extends TestCase
 {
 
     private RegisterFieldValidation $validation;
+    private MessageHelper $messageHelper;
 
     protected function setUp(): void
     {
         $this->validation = new RegisterFieldValidation();
+        $this->messageHelper = new MessageHelper();
 
         parent::setUp();
+    }
+
+    public function testCanInvalidateAllEmptyFields()
+    {
+        $account = new AccountModel();
+        $account->setEmail('');
+        $account->setUsername('');
+        $account->setPassword('');
+        $account->setAcceptedTerms(false);
+
+        $this->validation->validate($account, $this->messageHelper);
+
+        $this->assertSame(4, $this->messageHelper->countMessageByType('danger'));
+        $this->messageHelper->clearMessageArray();
     }
 
 }

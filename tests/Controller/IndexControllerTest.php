640bf11ac25e86b62271940d59e3278c10ec18aa
namePlayer <r06sollfrank@gmail.com>
2022-08-07 22:31:20 +0200

    Index Controller Test

--- tests/Controller/IndexControllerTest.php
+++ tests/Controller/IndexControllerTest.php
@@ -13,14 +13,36 @@ use Psr\Http\Message\ResponseInterface;
 class IndexControllerTest extends TestCase
 {
 
-    public function testLoadReturnsResponseInterface()
+    protected function setUp(): void
     {
         $_ENV['SOFTWARE_INDEV'] = true;
 
+        parent::setUp();
+    }
+
+    public function testLoadReturnsResponseInterface()
+    {
         $request = new Request();
 
         $indexController = new IndexController();
         $this->assertInstanceOf(ResponseInterface::class, $indexController->load($request));
+    }
+
+    public function testLoadReturnsSoftwareInfo()
+    {
+        $expected =
+            json_encode(
+                ['version' => Software::VERSION,
+                'build' => Software::VERSION_CODE,
+                'development' => (bool)$_ENV['SOFTWARE_INDEV']]
+            );
+
+        $request = new Request();
+        $indexController = new IndexController();
+        $response = $indexController->load($request);
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertSame($expected, (string)$response->getBody());
     }
 
 }

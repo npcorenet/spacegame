92f7ecd6d3a4ca34f5736073b2ac40c70bdc3cc8
namePlayer <r06sollfrank@gmail.com>
2022-08-08 21:10:07 +0200

    Fix Planet Distance

--- src/Table/PlanetTable.php
+++ src/Table/PlanetTable.php
@@ -8,7 +8,7 @@ class PlanetTable extends AbstractTable
 
     public function findPlanetsBySolarId(int $id): array|bool
     {
-        return $this->query->from($this->getTableName())->where(['solarSystem' => $id, 'moon' => null])->fetchAll();
+        return $this->query->from($this->getTableName())->where(['solarSystem' => $id, 'moon' => null])->orderBy('distance')->fetchAll();
     }
 
     public function findMoonsByPlanetId(int $id): array|bool

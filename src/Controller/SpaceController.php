c3a9c435b74c44a2ef104bb2dc9de24583c6d90a
namePlayer <r06sollfrank@gmail.com>
2022-08-08 22:09:39 +0200

    Code optimization

--- src/Controller/SpaceController.php
+++ src/Controller/SpaceController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Service\SpaceService;
 use App\Table\PlanetTable;
 use App\Table\SolarSystemTable;
 use Laminas\Diactoros\Response;
@@ -19,31 +20,14 @@ class SpaceController extends AbstractController
             return $this->response();
         }
 
-        $solarSystemTable = new SolarSystemTable($this->database);
-        $solarData = $solarSystemTable->findAll();
-
         if(!file_exists(CACHE_DIR.'/planets.json'))
         {
+            $solarSystemTable = new SolarSystemTable($this->database);
+            $planetTable = New PlanetTable($this->database);
 
-            foreach ($solarData as $solar) {
-                $spaceData[$solar['id']] = $solar;
-                unset($spaceData[$solar['id']]['id']);
-
-                $planetTable = new PlanetTable($this->database);
-                $planets = $planetTable->findPlanetsBySolarId($solar['id']);
-                foreach ($planets as $planet) {
-                    $spaceData[$solar['id']]['planets'][$planet['id']] = $planet;
-                    foreach ($planetTable->findMoonsByPlanetId($planet['id']) as $moon) {
-                        $spaceData[$solar['id']]['planets'][$planet['id']]['moons'][$moon['id']] = $moon;
-                        unset($spaceData[$solar['id']]['planets'][$planet['id']]['moons'][$moon['id']]['id']);
-                    }
-
-                    unset($spaceData[$solar['id']]['planets'][$planet['id']]['id']);
-                }
-            }
+            $spaceData = (new SpaceService($solarSystemTable, $planetTable))->generateSolarSystemArray();
 
             file_put_contents(CACHE_DIR.'/planets.json', json_encode($spaceData));
-
             $this->data = ['code' => 200, 'message' => self::CODE200, 'data' => $spaceData];
 
             return $this->response();
